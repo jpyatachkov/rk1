8f6efd02b0878df70f23662bd678f7329342805b
Кирилл Кучеров <cvkucherov@yandex.ru>
2018-12-14 18:10:00 +0300

    Done with bug

--- app/src/main/java/ru/mail/park/jpyatachkov/rk1/MainActivity.kt
+++ app/src/main/java/ru/mail/park/jpyatachkov/rk1/MainActivity.kt
@@ -2,11 +2,21 @@ package ru.mail.park.jpyatachkov.rk1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView.adapter = SingleNumberAdapter(numbersList, this)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    companion object {
+        private const val LIST_SIZE = 100
+        private val numbersList = (1..LIST_SIZE).toList().toIntArray()
     }
 }
---
+++ app/src/main/java/ru/mail/park/jpyatachkov/rk1/SingleNumberAdapter.kt
@@ -0,0 +1,58 @@
+package ru.mail.park.jpyatachkov.rk1
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.single_number.view.*
+
+class SingleNumberAdapter(
+    private val items: IntArray,
+    private val context: Context
+) : RecyclerView.Adapter<SingleNumberAdapter.SingleNumberViewHolder>() {
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): SingleNumberViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.single_number, viewGroup, false)
+        return SingleNumberViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: SingleNumberViewHolder, position: Int) {
+        viewHolder.numberTextView.text = items[position].toString()
+
+        viewHolder.minusButton.setOnClickListener {
+            --items[position]
+            updateItem(position, viewHolder)
+        }
+
+        viewHolder.plusButton.setOnClickListener {
+            ++items[position]
+            updateItem(position, viewHolder)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    private fun updateItem(position: Int, viewHolder: SingleNumberViewHolder) {
+        val item = items[position]
+        viewHolder.numberTextView.text = item.toString()
+        changeBackgroundColorByItem(item, viewHolder)
+    }
+
+    private fun changeBackgroundColorByItem(item: Int, viewHolder: SingleNumberViewHolder) {
+        val color = if (item >= 0) {
+            context.getColor(R.color.positiveBackground)
+        } else {
+            context.getColor(R.color.negativeBackground)
+        }
+        viewHolder.itemView.setBackgroundColor(color)
+    }
+
+    class SingleNumberViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val minusButton = view.minusButton!!
+        val numberTextView = view.numberTextView!!
+        val plusButton = view.plusButton!!
+    }
+}
